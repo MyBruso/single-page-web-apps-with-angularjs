ecb5db6be06ee107d33b2330ba10a78b0f306a6b
MyBruso <priyatalwalkar@yahoo.co.in>
2017-01-28 17:30:47 +0530

    Commiting week4 solution.

---
+++ module4-solution/src/menulist/itemcontroller.js
@@ -0,0 +1,9 @@
+(function(){
+	angular.module('MenuApp')
+		.controller('ItemController', ItemController);
+	ItemController.$inject = ['items'];
+	function ItemController(items) {
+		var itemCtrl = this;
+		itemCtrl.items = items;
+	}
+})();
